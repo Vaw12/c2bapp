bc0ca0b8e7819a6369a584b640bddd515d5e7041
Vaw12 <alvinmuli14@gmail.com>
2021-07-20 17:34:25 +0300

    Add Author Resource controller, seeders, model, factory and database migration

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+});
+
 Route::resource('products', ProductController::class);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -24,6 +30,9 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 */
 
+
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
